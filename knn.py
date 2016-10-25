2988ccc4810c20a606099e9eedc5142e5d0af274
israelcvidal <israelcvidal@gmail.com>
2016-10-24 23:21:25 -0300

    classes 'tools' and 'knn'created. knn: predict, fit and score methods added.

---
+++ knn.py
@@ -0,0 +1,99 @@
+import operator
+import tools as t
+class knn:
+	def __init__(self, n_neighbors):
+		self.k = n_neighbors
+
+	def getNeighbors(self, trfeatures, trlabels, current, k):
+		distances = []
+		neighbors = []
+		tools = t.tools()
+		#considering that current doesnt have a label anymore
+		length = len(current)
+		# if one wants to pass current with label: 
+		# length = len(current)-1
+
+		for x in range(len(trfeatures)):
+			dist = tools.euclidianDistance(current, trfeatures[x], length)
+			distances.append((trlabels[x], dist))
+
+		# sorting distance's list by dist
+		distances.sort(key=operator.itemgetter(1))
+
+		# getting k nearest neighbors
+		for x in range(k):
+			neighbors.append(distances[x][0])
+		return neighbors
+
+	def getResponse(self, neighbors):
+		classVotes = {}
+		for x in range(len(neighbors)):
+			# getting label of neighbor
+			response = neighbors[x]
+			if response in classVotes:
+				classVotes[response] += 1
+			else:
+				classVotes[response] = 1
+
+		#getting the most voted class 
+		sortedVotes = sorted(classVotes.items(), key=operator.itemgetter(1), reverse=True)
+		return sortedVotes[0][0]
+
+	def getAccuracy(self, labels, predictions):
+		correct = 0
+		for x in range(len(labels)):
+			if labels[x] == predictions[x]:
+				correct += 1
+		return (correct/float(len(labels)))
+
+	def fit(self, features, labels):
+		self.trfeatures = features[:]
+		self.trlabels = labels[:]
+
+	def predict(self, ttfeatures):
+		predictions = []
+		for x in range(len(ttfeatures)):
+			neighbors = self.getNeighbors(self.trfeatures, self.trlabels, ttfeatures[x], self.k)
+			result = self.getResponse(neighbors)
+			predictions.append(result)
+		return predictions
+
+
+	def score(self, ttfeatures, ttlabels):
+		predictions = self.predict(ttfeatures)
+		return self.getAccuracy(ttlabels, predictions)
+		
+
+def main():
+	tools = t.tools()
+	kn = knn(3)
+
+	tools.loadDataset('haberman.data')
+	trfeatures, trlabels, ttfeatures, ttlabels = tools.splitDataset()
+
+	kn.fit(trfeatures, trlabels)
+
+	score = kn.score(ttfeatures, ttlabels)
+	print score
+
+main()
+
+# knn = knn()
+
+# knn.fit([(2, 2, 2), (3, 3, 2), (4, 4, 4)], ['a', 'b', 'b'] )
+# print knn.getResponse(knn.getNeighbors(knn.trfeatures, (1,1,1) , 2))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- tools.py
+++ tools.py
@@ -1,74 +1,28 @@
-# import numpy as np
-import operator
+import numpy as np
 import math
 
-def loadDataset(filename):
-	data = np.loadtxt(filename, delimiter="n")
-	return data
+class tools(object):
+	def loadDataset(self, filename):
+		self.data = np.loadtxt(filename, delimiter=",")
 
-def splitDataset(data):
-	ndata = np.random.permutation(data)
+	def splitDataset(self):
+		ndata = np.random.permutation(self.data)
 
-	size = len(ndata)
-	nt = int(math.floor(sizer*0.7))
+		size = len(ndata)
+		nt = int(math.floor(size*0.7))
 
-	# Spliting the dataset into training and test
-	trfeatures = ndata[0:nt,0:3]
-	ttfeatures = ndata[nt:size, 0:3]
-	trlabels = ndata[0:nt,3]
-	ttlabels = ndata[nt:size, 3]
+		# Spliting the dataset into training and test
+		trfeatures = ndata[0:nt,0:3]
+		ttfeatures = ndata[nt:size, 0:3]
+		trlabels = ndata[0:nt,3]
+		ttlabels = ndata[nt:size, 3]
 
-	return trfeatures, trlabels, ttfeatures, ttlabels
+		return trfeatures, trlabels, ttfeatures, ttlabels
 
-def euclidianDistance(array1, array2, lenght):
-	# if(len(array1) != len(array2)):
-	# 	return
-	distance = 0
-	for x in range(lenght):
-		distance += pow((array1[x] - array2[x]), 2)
-	return math.sqrt(distance)
-
-def getNeighbors(dataset, current, k):
-	distances = []
-	neighbors = []
-
-	#considering that current doesnt have a label anymore
-	# length = len(current)
-	# if one wants to pass current with label: 
-	length = len(current)-1
-
-	for x in range(len(dataset)):
-		dist = euclidianDistance(current, dataset[x], length)
-		distances.append((dataset[x], dist))
-
-	# sorting distance's list by dist
-	distances.sort(key=operator.itemgetter(1))
-
-	# getting k nearest neighbors
-	for x in range(k):
-		neighbors.append(distances[x][0])
-	return neighbors
-
-def getResponse(neighbors):
-	classVotes = {}
-	for x in range(len(neighbors)):
-		# getting label of neighbor
-		response = neighbors[x][-1]
-		if response in classVotes:
-			classVotes[response] += 1
-		else:
-			classVotes[response] = 1
-
-	#getting the most voted class 
-	sortedVotes = sorted(classVotes.items(), key=operator.itemgetter(1), reverse=True)
-	return sortedVotes[0][0]
-
-
-def getAccuracy(dataset, predictions):
-	correct = 0
-	for x in range(len(dataset)):
-		if dataset[x][-1] == predictions[x]:
-			correct += 1
-	return (correct/float(len(testSet))) * 100.0
-
-
+	def euclidianDistance(self, array1, array2, lenght):
+		if(len(array1) != len(array2)):
+			return
+		distance = 0
+		for x in range(lenght):
+			distance += pow((array1[x] - array2[x]), 2)
+		return math.sqrt(distance)
