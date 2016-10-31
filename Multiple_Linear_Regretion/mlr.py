12d7a4ccaf95f84c7d897279eaaf186b15298417
israelcvidal <israelcvidal@gmail.com>
2016-10-31 17:50:39 -0300

    To implement: Multiple_Linear_Regretion

---
+++ Multiple_Linear_Regretion/mlr.py
@@ -0,0 +1,10 @@
+from sklearn.linear_model import SGDClassifier
+
+# X = [[0.,0.], [1.,1]]
+# y = [0.,1.]
+# clf = SGDClassifier(loss="hinge", penalty="l2")
+# clf.fit(X, y)
+# # print(clf.predict([[2., 2.]]))
+# print(clf.coef_)
+
+print("oi")
